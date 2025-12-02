28cc980852abe0f145a587cd411c1c103b85e917
Osamo <osamovicky2021@gmail.com>
2025-12-02 21:00:49 +0300

    patient management

---
+++ patient-service/src/main/java/com/pm/patientservice/dto/PatientResponseDTO.java
@@ -0,0 +1,9 @@
+package com.pm.patientservice.dto;
+
+public class PatientResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String address;
+    private String dateOfBirth;
+}
--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -1,11 +1,19 @@
 package com.pm.patientservice.service;
 
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
+
+import java.util.List;
 
 public class PatientService {
     private PatientRepository patientRepository;
 
     public PatientService(PatientRepository patientRepository) {
         this.patientRepository = patientRepository;
+    }
+
+    public List<PatientResponseDTO> getPatient(){
+        List<Patient> patientList = patientRepository.findAll();
     }
 }

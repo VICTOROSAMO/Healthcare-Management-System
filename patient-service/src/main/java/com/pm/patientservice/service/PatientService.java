04659170376ea2188bcb1617bb1aa2aa3ced55c2
Osamo <osamovicky2021@gmail.com>
2025-12-04 20:02:27 +0300

    patient management

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -39,7 +39,7 @@ public class PatientService {
     public PatientResponseDTO updatePatient(UUID id,PatientRequestDTO patientRequestDTO){
       Patient patient = patientRepository.findById(id).orElseThrow(()-> new PatientNotFoundException("Patient not found with id: " +id));
 
-        if(patientRepository.existsByEmail(patientRequestDTO.getEmail())){
+        if(patientRepository.existsByEmailAndIdNot(patientRequestDTO.getEmail(), id)){
             throw new EmailAlreadyExistsException("A patient with this email already exists"+ " " + patientRequestDTO.getEmail());
         }
         patient.setName(patientRequestDTO.getName());
@@ -51,5 +51,8 @@ public class PatientService {
         return PatientMapper.toDTO(updatedPatient);
     }
 
+    public void deletePatient(UUID id){
+        patientRepository.deleteById(id);
+    }
 
 }
